c9e9d3689e5593a040798c95ab94702678e28605
Ahmed Negm <ahmdn4418@gmail.com>
2024-11-24 04:09:20 +0200

    updates in design

--- src/components/ui/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './Input.module.scss'
-const Input = ({placeholder,onChange,value,error,type}) => {
-  return (
-    <input className={classes.input} value={value} onChange={onChange} type={type ? type : "text"} style={{border:error && '1px solid var(--redColor)'}} placeholder={placeholder} />
-  )
-}
-
-export default Input
